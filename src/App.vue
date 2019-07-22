61d58f3b29a6f384f104bca632504a121cbd0c40
masstroops <1139006439@qq.com>
2019-07-22 08:23:00 +0800

    2

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    大型贵重仪器申购论证表
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/axios/request.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+// 创建 axios 实例
+const service = axios.create({
+  baseURL: '/api', // api base_url
+  timeout: 6000 // 请求超时时间
+})
+
+export {
+  service as axios
+}
